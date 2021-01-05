dd159cce08ec637726afb4206fbb138787df1c5a
Basim <marifzee@hotmail.com>
2021-01-05 15:54:54 -0500

    added platform

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-
+var platform
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform = new Ground(150,305,300,170)
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -53,10 +53,12 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+   
     box5.display();
     log4.display();
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
